4acb271af6dea05ad1fe93a3fafd6529795df85d
Wheeler Weise <wheeler.weise@gmail.com>
2019-07-02 10:25:39 -0500

    Appears to be spawning threads, but no real speedup.

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,16 +2,20 @@
 from PIL import Image, ImageDraw
 import random, time
 import multiprocessing
-from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+from concurrent.futures import ThreadPoolExecutor
+import threading
 
 start_time=time.time()
-z=[None] * 80
-z[0]=0
+
 ''' In order to understand the following code, first look up the Mandlebrot Set
 You will soon come across lots of foreign terms like complex numbers and the complex plane
 Learn what those mean. Example of a complex number: (-1.9890625000000006 + 0.45624999999995547j) '''
 def mandelbrot(c):
 	# We do this 80 times to make sure a certain complex number is indeed part of the Mandlebrot Set
+	z=[None] * 80
+	z[0]=0
+	
 	for n in range(1,80):
 		z[n] = z[n-1]**2 + c
 		# If it's distance is greater than 2 from (0,0) NOT part of set
@@ -73,23 +77,30 @@ for i in range(max_workers):
 	else:
 		width_chunks.append(range(i*chunk_size, (i+1)*chunk_size))
 
-for i in range(width):
-	for j in range(height):
-		''' 
-		First loop: iterate over pixel (0,0),(0,1)...(0,1079),(0,1080)
-		Second loop: iterate over pixel (1,0),(1,1)...(1,1079),(1,1080)
-		2nd last loop: iterate over pixel (1979,0),(1979,1)...(1979,1079),(1979,1080)
-		Last loop: iterate over pixel (1980,0),(1980,1)...(1980,1079),(1980,1080) '''
-		c = complex(realPlots[i],imagiPlots[j])
-		# n is returned from the mandelbrot function and assigned to varaible
-		n = mandelbrot(c)
+def process_chunk(chunk_range):
+	print("thread " + str(threading.get_ident()))
+	chunk_results = []
+	for i in chunk_range:
+		for j in range(height):
+			c = complex(realPlots[i],imagiPlots[j])
+			# n is returned from the mandelbrot function and assigned to varaible
+			n = mandelbrot(c)
 
-		# If n is 80, do nothing and the pixel remains black(Our canvas value). Otherwise do the following
-		if n != 80:
-			''' Depending on n, if it is a HIGH VALUE (like 60) color will be darker(255 = white) (0 = black)
-			if n is LOW (like 5), color will be lighter.'''
-			color = (255 - int(n * 3.1875))
-			draw.point(([i,j]), (color, color, color-random.randint(50,70)))
+			# If n is 80, do nothing and the pixel remains black(Our canvas value). Otherwise do the following
+			if n != 80:
+				''' Depending on n, if it is a HIGH VALUE (like 60) color will be darker(255 = white) (0 = black)
+				if n is LOW (like 5), color will be lighter.'''
+				color = (255 - int(n * 3.1875))
+			# draw.point(([i,j]), (color, color, color-random.randint(50,70)))
+			result = (([i,j]), (color, color, color-random.randint(50,70)))
+			chunk_results.append(result)
+	return chunk_results
+
+executor = ThreadPoolExecutor(max_workers)
+result_chunks = executor.map(process_chunk, width_chunks)
+for result_chunk in result_chunks:
+	for result in result_chunk:
+		draw.point(result[0], result[1])
 
 im.save('output.png', 'PNG')
 print("--- %s seconds ---" % (time.time() - start_time))
